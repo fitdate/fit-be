bc41b57ae9887f59068fd4ad2051e6a7293d0f72
Ryuji <hi_ryuji@naver.com>
2025-04-11 11:22:54 +0900

    feat: 프로젝트 기본 설정 - TypeORM 데이터베이스 연결 설정 - 환경 변수 설정 - 개발 도구 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,9 +17,52 @@ import { InterestLocationModule } from './modules/profile/interest-location/inte
 import { IntroductionModule } from './modules/profile/introduction/introduction.module';
 import { MbtiModule } from './modules/profile/mbti/mbti.module';
 import { ProfileImageModule } from './modules/profile/profile-image/profile-image.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { configuration } from './common/config/configuration';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AllConfig } from './common/config/config.types';
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
+import { User } from './modules/user/entities/user.entity';
+import { InterestLocation } from './modules/profile/interest-location/entities/interest-location.entity';
+import { InterestCategory } from './modules/profile/interest-category/entities/interest-category.entity';
+import { ProfileImage } from './modules/profile/profile-image/entities/profile-image.entity';
+import { join } from 'path';
+import { DevtoolsModule } from '@nestjs/devtools-integration';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+      envFilePath: '.env.dev',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (
+        configService: ConfigService<AllConfig>,
+      ): PostgresConnectionOptions => ({
+        type: 'postgres',
+        host: configService.getOrThrow('database.host', { infer: true }),
+        port: configService.getOrThrow('database.port', { infer: true }),
+        username: configService.getOrThrow('database.username', {
+          infer: true,
+        }),
+        password: configService.getOrThrow('database.password', {
+          infer: true,
+        }),
+        database: configService.getOrThrow('database.name', {
+          infer: true,
+        }),
+        entities: [User, InterestLocation, InterestCategory, ProfileImage],
+        migrations: [join(__dirname, '/**/*.migration{.ts,.js}')],
+        synchronize: true,
+        logging: true,
+      }),
+      inject: [ConfigService],
+    }),
+    DevtoolsModule.register({
+      http: process.env.dev !== 'production',
+    }),
     AuthModule,
     ChatRoomModule,
     LikeModule,
