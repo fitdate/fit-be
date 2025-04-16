5a8dccfd830dd22790d49174af8077d5ec874775
Ryuji <hi_ryuji@naver.com>
2025-04-16 22:41:41 +0900

    fix: 헬스 체크 12회에서 3회로 수정

--- src/modules/payment/payment.module.ts
+++ src/modules/payment/payment.module.ts
@@ -4,9 +4,10 @@ import { Payment } from './entities/payment.entity';
 import { PaymentService } from './payment.service';
 import { PaymentController } from './payment.controller';
 import { UserModule } from '../user/user.module';
+import { User } from '../user/entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Payment]), UserModule],
+  imports: [TypeOrmModule.forFeature([Payment, User]), UserModule],
   controllers: [PaymentController],
   providers: [PaymentService],
   exports: [PaymentService],
