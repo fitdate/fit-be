4d65d51117b87009ba58ad4e5415f54ec1469973
Ryuji <hi_ryuji@naver.com>
2025-04-16 22:29:51 +0900

    fix: UserStatisticsService 의존성 주입 문제 해결

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -4,9 +4,11 @@ import { UserController } from './user.controller';
 import { User } from './entities/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserStatisticsService } from './services/user-statistics.service';
+import { Profile } from '../profile/entities/profile.entity';
+import { InterestLocation } from '../profile/interest-location/entities/interest-location.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, Profile, InterestLocation])],
   controllers: [UserController],
   providers: [UserService, UserStatisticsService],
   exports: [UserService, UserStatisticsService],
