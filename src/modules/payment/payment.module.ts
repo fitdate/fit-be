a94eba23f1d0aa32a90379c41c5ae801ccb65d52
Ryuji <hi_ryuji@naver.com>
2025-04-16 22:36:55 +0900

    fix: 의존성 주입 오류 수정

--- src/modules/payment/payment.module.ts
+++ src/modules/payment/payment.module.ts
@@ -3,10 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Payment } from './entities/payment.entity';
 import { PaymentService } from './payment.service';
 import { PaymentController } from './payment.controller';
-import { User } from '../user/entities/user.entity';
+import { UserModule } from '../user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Payment, User])],
+  imports: [TypeOrmModule.forFeature([Payment]), UserModule],
   controllers: [PaymentController],
   providers: [PaymentService],
   exports: [PaymentService],
