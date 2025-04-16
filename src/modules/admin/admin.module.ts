1ecefa8548f0116202b683a7b082f0bd169d8bda
Ryuji <hi_ryuji@naver.com>
2025-04-16 22:48:05 +0900

    fix: AdminModule 의존성 설정 수정

--- src/modules/admin/admin.module.ts
+++ src/modules/admin/admin.module.ts
@@ -4,9 +4,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Admin } from './entities/admin.entity';
 import { UserModule } from '../user/user.module';
 import { AdminService } from './admin.service';
+import { PaymentModule } from '../payment/payment.module';
+import { User } from '../user/entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Admin]), UserModule],
+  imports: [TypeOrmModule.forFeature([Admin, User]), UserModule, PaymentModule],
   controllers: [AdminController],
   providers: [AdminService],
   exports: [AdminService],
