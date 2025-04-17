2c36b07a8cc3b125e59d299e45272096ea109644
Ryuji <hi_ryuji@naver.com>
2025-04-17 09:11:51 +0900

    fix: 배포 시 기존 테이블 삭제 후 재생성 로직 추가

--- src/modules/chat-room/entities/chat-room-user.entity.ts
+++ src/modules/chat-room/entities/chat-room-user.entity.ts
@@ -1,10 +1,4 @@
-import {
-  Entity,
-  ManyToOne,
-  JoinColumn,
-  PrimaryGeneratedColumn,
-  Column,
-} from 'typeorm';
+import { Entity, ManyToOne, JoinColumn, PrimaryColumn } from 'typeorm';
 import { ChatRoom } from './chat-room.entity';
 import { User } from '../../user/entities/user.entity';
 import { ApiProperty } from '@nestjs/swagger';
@@ -12,15 +6,11 @@ import { ApiProperty } from '@nestjs/swagger';
 @Entity('chat_room_users')
 export class ChatRoomUser {
   @ApiProperty()
-  @PrimaryGeneratedColumn('uuid')
-  id: string;
-
-  @ApiProperty()
-  @Column('uuid')
+  @PrimaryColumn('uuid')
   chatRoomId: string;
 
   @ApiProperty()
-  @Column('uuid')
+  @PrimaryColumn('uuid')
   userId: string;
 
   @ApiProperty({ type: () => ChatRoom })
