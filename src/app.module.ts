d8f74c61dc3c3835f392a54b0814c7f9227c5d01
Ryuji <hi_ryuji@naver.com>
2025-04-18 12:02:13 +0900

    fix: DB 자동 동기화 및 초기화 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -90,7 +90,7 @@ import { AdminController } from './modules/admin/admin.controller';
         entities: [join(__dirname, '/**/*.entity{.ts,.js}')],
         migrations: [join(__dirname, '/migration/*{.ts,.js}')],
         migrationsRun: true,
-        synchronize: false,
+        synchronize: true,
         logging: true,
       }),
       inject: [ConfigService],
