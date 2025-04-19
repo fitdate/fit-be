bcb6a981b9d7d680e13d6795c2408931db80110a
Ryuji <hi_ryuji@naver.com>
2025-04-19 20:21:47 +0900

    test: 스웨거 테스트

--- src/modules/mailer/mailer.module.ts
+++ src/modules/mailer/mailer.module.ts
@@ -3,28 +3,27 @@ import { MailerService } from './mailer.service';
 import { MailerController } from './mailer.controller';
 import { MailerModule as NestMailerModule } from '@nestjs-modules/mailer';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { AllConfig } from '../../common/config/config.types';
+import { AllConfig, MailerConfig } from '../../common/config/config.types';
 
 @Module({
   imports: [
     NestMailerModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: (configService: ConfigService<AllConfig>) => ({
-        transport: {
-          host: configService.getOrThrow('mailer.MAILER_HOST', { infer: true }),
-          port: configService.getOrThrow('mailer.MAILER_PORT', { infer: true }),
-          secure: true,
-          auth: {
-            user: configService.getOrThrow('mailer.MAILER_USER', {
-              infer: true,
-            }),
-            pass: configService.getOrThrow('mailer.MAILER_PASSWORD', {
-              infer: true,
-            }),
+      useFactory: (configService: ConfigService<AllConfig>) => {
+        const mailerConfig = configService.getOrThrow<MailerConfig>('mailer');
+        return {
+          transport: {
+            host: mailerConfig.MAILER_HOST,
+            port: mailerConfig.MAILER_PORT,
+            secure: true,
+            auth: {
+              user: mailerConfig.MAILER_USER,
+              pass: mailerConfig.MAILER_PASSWORD,
+            },
           },
-        },
-      }),
+        };
+      },
     }),
   ],
   controllers: [MailerController],
