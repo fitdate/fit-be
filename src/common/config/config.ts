588c1c48560a6ced9447ffb5c782e5ec9128c03d
Ryuji <hi_ryuji@naver.com>
2025-04-19 15:34:57 +0900

    test: 배포 테스트

--- src/common/config/config.ts
+++ src/common/config/config.ts
@@ -101,13 +101,13 @@ export const validationSchema = Joi.object({
   GOOGLE_CLIENT_ID: Joi.string().required(),
   GOOGLE_CLIENT_SECRET: Joi.string().required(),
   GOOGLE_CALLBACK_URL: Joi.string().required(),
-  KAKAO_CLIENT_ID: Joi.string().required(),
-  KAKAO_CLIENT_SECRET: Joi.string().required(),
-  KAKAO_CALLBACK_URL: Joi.string().required(),
-  NAVER_CLIENT_ID: Joi.string().required(),
-  NAVER_CLIENT_SECRET: Joi.string().required(),
-  NAVER_CALLBACK_URL: Joi.string().required(),
-  SOCIAL_FRONTEND_URL: Joi.string().required(),
+  // KAKAO_CLIENT_ID: Joi.string().required(),
+  // KAKAO_CLIENT_SECRET: Joi.string().required(),
+  // KAKAO_CALLBACK_URL: Joi.string().required(),
+  // NAVER_CLIENT_ID: Joi.string().required(),
+  // NAVER_CLIENT_SECRET: Joi.string().required(),
+  // NAVER_CALLBACK_URL: Joi.string().required(),
+  // SOCIAL_FRONTEND_URL: Joi.string().required(),
 });
 
 export const ConfigModuleOptions = {
