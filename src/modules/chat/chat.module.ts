48a338d883d084f09083faa14d5d927ae2f33293
Ryuji <hi_ryuji@naver.com>
2025-04-21 14:53:16 +0900

    fix: chat 모듈 의존성 주입 오류 해결

--- src/modules/chat/chat.module.ts
+++ src/modules/chat/chat.module.ts
@@ -5,9 +5,10 @@ import { ChatGateway } from './chat.gateway';
 import { ChatController } from './chat.controller';
 import { ChatMessage } from './entities/chat-message.entity';
 import { ChatRoom } from './entities/chat-room.entity';
+import { UserModule } from '../user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ChatMessage, ChatRoom])],
+  imports: [TypeOrmModule.forFeature([ChatMessage, ChatRoom]), UserModule],
   controllers: [ChatController],
   providers: [ChatService, ChatGateway],
   exports: [ChatService],
