1ac8897ff1d2dcea8482a8c3cc4fb9066d6026d6
sunghan <rlfjrlek@gmail.com>
2025-04-21 19:01:10 +0900

    fix: profile-img

--- src/modules/profile/profile-image/dto/create-profile-image.dto.ts
+++ src/modules/profile/profile-image/dto/create-profile-image.dto.ts
@@ -1,20 +1,25 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsArray, IsOptional, IsString } from 'class-validator';
+import { IsArray, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateProfileImageDto {
   @ApiProperty({
-    description: '프로필 이미지들',
+    description: '프로필 이미지 이름들',
     example: [
       'profile-image-1.jpg',
       'profile-image-2.jpg',
       'profile-image-3.jpg',
     ],
   })
-  @IsOptional()
   @IsArray()
   @IsString({ each: true })
-  profileImageName?: string[];
+  @IsNotEmpty()
+  profileImageName: string[];
 
+  @ApiProperty({
+    description: '프로필 ID',
+    example: '123e4567-e89b-12d3-a456-426614174000',
+  })
   @IsString()
+  @IsNotEmpty()
   profileId: string;
 }
