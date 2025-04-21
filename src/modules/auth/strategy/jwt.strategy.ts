9c141fd518d7bdd3da4bbc1c4abe214a6975241a
Ryuji <hi_ryuji@naver.com>
2025-04-21 19:01:32 +0900

    fix: JWT 인증 전략을 헤더 대신 쿠키 기반으로 변경

--- src/modules/auth/strategy/jwt.strategy.ts
+++ src/modules/auth/strategy/jwt.strategy.ts
@@ -2,14 +2,25 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { AllConfig } from 'src/common/config/config.types';
 import { AuthGuard, PassportStrategy } from '@nestjs/passport';
-import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Strategy } from 'passport-jwt';
 import { TokenPayload } from '../types/token-payload.types';
+import { Request } from 'express';
+
+const cookieExtractor = (req: Request) => {
+  let token: string | null = null;
+  if (req && req.cookies) {
+    token = req.cookies.accessToken as string;
+  }
+  return token;
+};
+
 export class JwtAuthGuard extends AuthGuard('jwt') {}
+
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
   constructor(private configService: ConfigService<AllConfig>) {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest: cookieExtractor,
       ignoreExpiration: false,
       secretOrKey: configService.getOrThrow('jwt.accessTokenSecret', {
         infer: true,
