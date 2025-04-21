72d7086f23356f8547472a823b780aafaf71d5ef
Ryuji <hi_ryuji@naver.com>
2025-04-21 18:32:46 +0900

    fix: cookie-parser import 방식 수정

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { TossPaymentResponse } from './modules/payment/types/toss-payment.types';
 import { RequestMethod } from '@nestjs/common';
-import cookieParser from 'cookie-parser';
+import * as cookieParser from 'cookie-parser';
 import { Request, Response, NextFunction } from 'express';
 
 async function bootstrap() {
