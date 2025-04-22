b888db74b9a5eb3076497eb72c2488f7ec9716b1
sunghan <rlfjrlek@gmail.com>
2025-04-22 17:22:12 +0900

    fix: 미들웨어 임시 비활성화, 불필요한 검증 제거

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import {
   MiddlewareConsumer,
   Module,
   NestModule,
-  RequestMethod,
+  // RequestMethod,
 } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { UserModule } from './modules/user/user.module';
@@ -26,7 +26,7 @@ import { config, validationSchema } from './common/config/config';
 import { AllConfig } from './common/config/config.types';
 import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 import { AuthGuard } from './modules/auth/guard/auth.guard';
-import { AuthMiddleware } from './modules/auth/middleware/auth.middleware';
+// import { AuthMiddleware } from './modules/auth/middleware/auth.middleware';
 import { DevtoolsModule } from '@nestjs/devtools-integration';
 import { LocationModule } from './modules/location/location.module';
 import { InterestCategoryModule } from './modules/profile/interest-category/common/interest-category.module';
@@ -35,26 +35,26 @@ import { SeedManagerModule } from './modules/seed/seed-manager.module';
 import { S3Module } from './modules/s3/s3.module';
 import { MailerModule } from './modules/mailer/mailer.module';
 import { AdminModule } from './modules/admin/admin.module';
-import { AuthController } from './modules/auth/auth.controller';
-import { UserController } from './modules/user/user.controller';
-import { ProfileController } from './modules/profile/profile.controller';
-import { MatchController } from './modules/match/match.controller';
-import { LikeController } from './modules/like/like.controller';
-import { PassController } from './modules/pass/pass.controller';
-import { NotificationController } from './modules/notification/notification.controller';
-import { PaymentController } from './modules/payment/payment.controller';
-import { InterestCategoryController } from './modules/profile/interest-category/common/interest-category.controller';
-import { MbtiController } from './modules/profile/mbti/mbti.controller';
-import { ProfileImageController } from './modules/profile/profile-image/profile-image.controller';
-import { FeedbackController } from './modules/profile/feedback/common/feedback.controller';
-import { IntroductionController } from './modules/profile/introduction/common/introduction.controller';
-import { LocationController } from './modules/location/location.controller';
-import { SeedManagerController } from './modules/seed/seed-manager.controller';
-import { S3Controller } from './modules/s3/s3.controller';
-import { MailerController } from './modules/mailer/mailer.controller';
-import { AdminController } from './modules/admin/admin.controller';
+// import { AuthController } from './modules/auth/auth.controller';
+// import { UserController } from './modules/user/user.controller';
+// import { ProfileController } from './modules/profile/profile.controller';
+// import { MatchController } from './modules/match/match.controller';
+// import { LikeController } from './modules/like/like.controller';
+// import { PassController } from './modules/pass/pass.controller';
+// import { NotificationController } from './modules/notification/notification.controller';
+// import { PaymentController } from './modules/payment/payment.controller';
+// import { InterestCategoryController } from './modules/profile/interest-category/common/interest-category.controller';
+// import { MbtiController } from './modules/profile/mbti/mbti.controller';
+// import { ProfileImageController } from './modules/profile/profile-image/profile-image.controller';
+// import { FeedbackController } from './modules/profile/feedback/common/feedback.controller';
+// import { IntroductionController } from './modules/profile/introduction/common/introduction.controller';
+// import { LocationController } from './modules/location/location.controller';
+// import { SeedManagerController } from './modules/seed/seed-manager.controller';
+// import { S3Controller } from './modules/s3/s3.controller';
+// import { MailerController } from './modules/mailer/mailer.controller';
+// import { AdminController } from './modules/admin/admin.controller';
 import { ChatModule } from './modules/chat/chat.module';
-import { ChatController } from './modules/chat/chat.controller';
+// import { ChatController } from './modules/chat/chat.controller';
 import { UserListModule } from './modules/user-list/user-list.module';
 import { Reflector } from '@nestjs/core';
 
@@ -151,46 +151,56 @@ import { Reflector } from '@nestjs/core';
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(AuthMiddleware)
-      .exclude(
-        { path: 'auth/login', method: RequestMethod.POST },
-        { path: 'auth/register', method: RequestMethod.POST },
-        { path: 'auth/send-verification-email', method: RequestMethod.POST },
-        { path: 'auth/verify-email', method: RequestMethod.POST },
-        { path: 'auth/google', method: RequestMethod.GET },
-        { path: 'auth/google/login/callback', method: RequestMethod.GET },
-        { path: 'auth/kakao', method: RequestMethod.GET },
-        { path: 'auth/kakao/login/callback', method: RequestMethod.GET },
-        { path: 'auth/naver', method: RequestMethod.GET },
-        { path: 'auth/naver/login/callback', method: RequestMethod.GET },
-        { path: 'health', method: RequestMethod.GET },
-        { path: 'docs', method: RequestMethod.GET },
-        { path: 'auth/check-email', method: RequestMethod.POST },
-        { path: 'auth/check-nickname', method: RequestMethod.POST },
-        { path: 'auth/email-login', method: RequestMethod.POST },
-      )
-      .forRoutes(
-        AuthController,
-        UserController,
-        ProfileController,
-        MatchController,
-        LikeController,
-        PassController,
-        NotificationController,
-        PaymentController,
-        InterestCategoryController,
-        MbtiController,
-        ProfileImageController,
-        FeedbackController,
-        IntroductionController,
-        LocationController,
-        SeedManagerController,
-        S3Controller,
-        MailerController,
-        AdminController,
-        AppController,
-        ChatController,
-      );
+    // consumer
+    //   .apply(AuthMiddleware)
+    //   .exclude(
+    //     { path: 'auth/login', method: RequestMethod.POST },
+    //     { path: 'auth/register', method: RequestMethod.POST },
+    //     { path: 'auth/send-verification-email', method: RequestMethod.POST },
+    //     { path: 'auth/verify-email', method: RequestMethod.POST },
+    //     { path: 'auth/google', method: RequestMethod.GET },
+    //     { path: 'auth/google/login/callback', method: RequestMethod.GET },
+    //     { path: 'auth/kakao', method: RequestMethod.GET },
+    //     { path: 'auth/kakao/login/callback', method: RequestMethod.GET },
+    //     { path: 'auth/naver', method: RequestMethod.GET },
+    //     { path: 'auth/naver/login/callback', method: RequestMethod.GET },
+    //     { path: 'health', method: RequestMethod.GET },
+    //     { path: 'docs', method: RequestMethod.GET },
+    //     { path: 'auth/check-email', method: RequestMethod.POST },
+    //     { path: 'auth/check-nickname', method: RequestMethod.POST },
+    //     { path: 'auth/email-login', method: RequestMethod.POST },
+    //     { path: 'auth/email-temp-register', method: RequestMethod.POST },
+    //     { path: 'auth/email-temp-register/verify', method: RequestMethod.POST },
+    //     {
+    //       path: 'auth/email-temp-register/verify-email',
+    //       method: RequestMethod.POST,
+    //     },
+    //     {
+    //       path: 'auth/email-temp-register/verify-email',
+    //       method: RequestMethod.POST,
+    //     },
+    //   )
+    //   .forRoutes(
+    //     AuthController,
+    //     UserController,
+    //     ProfileController,
+    //     MatchController,
+    //     LikeController,
+    //     PassController,
+    //     NotificationController,
+    //     PaymentController,
+    //     InterestCategoryController,
+    //     MbtiController,
+    //     ProfileImageController,
+    //     FeedbackController,
+    //     IntroductionController,
+    //     LocationController,
+    //     SeedManagerController,
+    //     S3Controller,
+    //     MailerController,
+    //     AdminController,
+    //     AppController,
+    //     ChatController,
+    //   );
   }
 }
