46ae075168c63ed099af504aad459ab349748431
Ryuji <hi_ryuji@naver.com>
2025-04-23 10:09:33 +0900

    fix: matchCode 제거

--- src/modules/match/entities/match.entity.ts
+++ src/modules/match/entities/match.entity.ts
@@ -1,10 +1,4 @@
-import {
-  Entity,
-  PrimaryGeneratedColumn,
-  ManyToOne,
-  Column,
-  JoinColumn,
-} from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, ManyToOne, JoinColumn } from 'typeorm';
 import { Profile } from '../../profile/entities/profile.entity';
 import { BaseTable } from '../../../common/entity/base-table.entity';
 import { ApiProperty } from '@nestjs/swagger';
@@ -14,10 +8,6 @@ export class Match extends BaseTable {
   @ApiProperty()
   @PrimaryGeneratedColumn('uuid')
   id: string;
-
-  @ApiProperty()
-  @Column({ unique: true })
-  matchCode: string;
 
   @ApiProperty({ type: () => Profile })
   @ManyToOne(() => Profile, { eager: true })
