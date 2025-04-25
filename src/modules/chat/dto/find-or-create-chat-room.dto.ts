97c984dd3619d58d049997033ec399ebe8f3e13a
Ryuji <hi_ryuji@naver.com>
2025-04-25 16:18:46 +0900

    chore: 오타 수정

--- src/modules/chat/dto/find-or-create-chat-room.dto.ts
+++ src/modules/chat/dto/find-or-create-chat-room.dto.ts
@@ -4,7 +4,7 @@ import { IsUUID } from 'class-validator';
 export class FindOrCreateChatRoomDto {
   @ApiProperty({
     description: '채팅방 상대방 사용자 ID',
-    example: 'user-uuid',
+    example: 'partner-uuid',
     type: 'string',
     format: 'uuid',
   })
