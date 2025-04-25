87d5595f8e54ca7dedfb4a8e623edde027f52fe3
sunghan <rlfjrlek@gmail.com>
2025-04-25 16:35:49 +0900

    fix: festival import number 로 변경

--- src/modules/festival/festival.controller.ts
+++ src/modules/festival/festival.controller.ts
@@ -7,7 +7,7 @@ export class FestivalController {
   constructor(private readonly festivalService: FestivalService) {}
 
   @Get()
-  getFestivalByRegion(@Query('region') region: string): Promise<FestivalDto[]> {
+  getFestivalByRegion(@Query('region') region: number): Promise<FestivalDto[]> {
     return this.festivalService.getFestivalByRegion(region);
   }
 }
