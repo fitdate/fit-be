1cebc458e7940fc4cdbd486d1cf5dc463e90e0e0
sunghan <rlfjrlek@gmail.com>
2025-05-07 22:39:37 +0900

    fix: dependenty 수정

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -10,12 +10,24 @@ import { CursorPaginationUtil } from 'src/common/util/cursor-pagination.util';
 import { RedisModule } from '../redis/redis.module';
 import { HashService } from '../auth/hash/hash.service';
 import { BcryptService } from '../auth/hash/bcrypt.service';
+import { ProfileImageModule } from '../profile/profile-image/profile-image.module';
+import { ProfileModule } from '../profile/profile.module';
+import { FeedbackModule } from '../profile/feedback/common/feedback.module';
+import { IntroductionModule } from '../profile/introduction/common/introduction.module';
+import { InterestCategoryModule } from '../profile/interest-category/common/interest-category.module';
+import { S3Module } from '../s3/s3.module';
 @Module({
   imports: [
     TypeOrmModule.forFeature([User]),
     LocationModule,
     FilterModule,
     RedisModule,
+    ProfileImageModule,
+    ProfileModule,
+    FeedbackModule,
+    IntroductionModule,
+    InterestCategoryModule,
+    S3Module,
   ],
   controllers: [UserController],
   providers: [
