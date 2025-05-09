3cf68361129e7565c10209f8dfb553469676c353
sunghan <rlfjrlek@gmail.com>
2025-05-09 15:11:02 +0900

    fix: 회원탈퇴 softdelete 로 변경, user entity에서 delete cascade
    - 하위 항목들 deletedat 추가

--- src/modules/profile/entities/profile.entity.ts
+++ src/modules/profile/entities/profile.entity.ts
@@ -5,46 +5,53 @@ import {
   OneToOne,
   PrimaryGeneratedColumn,
 } from 'typeorm';
-// import { InterestLocation } from '../interest-location/entities/interest-location.entity';
 import { ProfileImage } from '../profile-image/entities/profile-image.entity';
 import { User } from '../../user/entities/user.entity';
 import { Mbti } from '../mbti/entities/mbti.entity';
 import { UserFeedback } from '../feedback/entities/user-feedback.entity';
 import { UserInterestCategory } from '../interest-category/entities/user-interest-category.entity';
 import { UserIntroduction } from '../introduction/entities/user-introduction.entity';
+import { BaseTable } from '../../../common/entity/base-table.entity';
 
 @Entity()
-export class Profile {
+export class Profile extends BaseTable {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
   @OneToMany(
     () => UserIntroduction,
     (userIntroduction) => userIntroduction.profile,
+    {
+      onDelete: 'CASCADE',
+    },
   )
   userIntroductions: UserIntroduction[];
 
-  @OneToMany(() => UserFeedback, (userFeedback) => userFeedback.profile)
+  @OneToMany(() => UserFeedback, (userFeedback) => userFeedback.profile, {
+    onDelete: 'CASCADE',
+  })
   userFeedbacks: UserFeedback[];
 
-  @OneToMany(() => ProfileImage, (profileImage) => profileImage.profile)
+  @OneToMany(() => ProfileImage, (profileImage) => profileImage.profile, {
+    onDelete: 'CASCADE',
+  })
   profileImage: ProfileImage[];
 
-  @OneToOne(() => Mbti, (mbti) => mbti.profile, { nullable: true })
+  @OneToOne(() => Mbti, (mbti) => mbti.profile, {
+    nullable: true,
+    onDelete: 'CASCADE',
+  })
   @JoinColumn()
   mbti: Mbti;
 
   @OneToMany(
     () => UserInterestCategory,
     (userInterestCategory) => userInterestCategory.profile,
+    {
+      onDelete: 'CASCADE',
+    },
   )
   interestCategory: UserInterestCategory[];
-
-  // @OneToMany(
-  //   () => InterestLocation,
-  //   (interestLocation) => interestLocation.profile,
-  // )
-  // interestLocation: InterestLocation[];
 
   @OneToOne(() => User, (user) => user.profile)
   user: User;
--- src/modules/profile/interest-category/entities/user-interest-category.entity.ts
+++ src/modules/profile/interest-category/entities/user-interest-category.entity.ts
@@ -1,9 +1,9 @@
 import { Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { InterestCategory } from './interest-category.entity';
 import { Profile } from '../../entities/profile.entity';
-
+import { BaseTable } from 'src/common/entity/base-table.entity';
 @Entity('user_interest_category')
-export class UserInterestCategory {
+export class UserInterestCategory extends BaseTable {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
