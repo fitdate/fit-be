53ed2148438a88fae8f23720393ae26f7a7c95b5
sunghan <rlfjrlek@gmail.com>
2025-05-09 17:05:23 +0900

    fix: user-feedback basetable 추가

--- src/modules/profile/feedback/entities/user-feedback.entity.ts
+++ src/modules/profile/feedback/entities/user-feedback.entity.ts
@@ -1,9 +1,9 @@
 import { Entity, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
 import { Profile } from '../../entities/profile.entity';
 import { Feedback } from './feedback.entity';
-
+import { BaseTable } from 'src/common/entity/base-table.entity';
 @Entity()
-export class UserFeedback {
+export class UserFeedback extends BaseTable {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
