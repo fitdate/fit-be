521f67cb43c18c2b5454c02d811d30f086d89539
sunghan <rlfjrlek@gmail.com>
2025-05-12 17:12:49 +0900

    fix: 데이터 시딩 아무나 가능하게 변경

--- src/modules/seed/seed-manager.controller.ts
+++ src/modules/seed/seed-manager.controller.ts
@@ -3,14 +3,15 @@ import { SeedManagerService } from './seed-manager.service';
 import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { RBAC } from 'src/common/decorator/rbac.decorator';
 import { UserRole } from 'src/common/enum/user-role.enum';
-
+import { Public } from 'src/common/decorator/public.decorator';
 @ApiTags('Seed Manager')
 @Controller('seed-manager')
 export class SeedManagerController {
   constructor(private readonly seedManagerService: SeedManagerService) {}
 
   @Post('insert')
-  @RBAC(UserRole.ADMIN)
+  @Public()
+  // @RBAC(UserRole.ADMIN)
   @ApiOperation({ summary: '삽입' })
   @ApiResponse({ status: 200, description: '삽입 성공' })
   @ApiResponse({ status: 400, description: '삽입 실패' })
@@ -19,7 +20,8 @@ export class SeedManagerController {
   }
 
   @Post('extract')
-  @RBAC(UserRole.ADMIN)
+  // @RBAC(UserRole.ADMIN)
+  @Public()
   @ApiOperation({ summary: '추출' })
   @ApiResponse({ status: 200, description: '추출 성공' })
   @ApiResponse({ status: 400, description: '추출 실패' })
