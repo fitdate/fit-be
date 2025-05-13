98ae46f27390c8c4fd4a718604eafdaa328dca8d
sunghan <rlfjrlek@gmail.com>
2025-05-13 15:55:38 +0900

    fix: session 의존성

--- src/modules/session/session.module.ts
+++ src/modules/session/session.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { SessionService } from './session.service';
 import { RedisModule } from '../redis/redis.module';
+import { SessionGateway } from './session.gateway';
+import { JwtModule } from '@nestjs/jwt';
 @Module({
-  imports: [RedisModule],
-  providers: [SessionService],
+  imports: [RedisModule, JwtModule],
+  providers: [SessionService, SessionGateway],
   exports: [SessionService],
 })
 export class SessionModule {}
